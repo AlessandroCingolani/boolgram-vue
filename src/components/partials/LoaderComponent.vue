fd3987880df709e838d0a0b854621cd9932efedb
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-19 15:50:42 +0100

    [init]:loader component

---
+++ src/components/partials/LoaderComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "LoaderComponent",
+};
+</script>
+
+<template>
+  <div class="loader">
+    <img src="/img/spinner.gif" alt="" class="load" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
