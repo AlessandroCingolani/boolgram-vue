0067d243593a1a19066be7c773687648ba6189c1
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-19 17:01:33 +0100

    [add]:axios and take data posts and profiles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
 import HeaderNav from "./components/HeaderNav.vue";
 import MainContent from "./components/MainContent.vue";
+import { store } from "./data/store";
+import axios from "axios";
 export default {
   name: "App",
   components: {
     HeaderNav,
     MainContent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getApi(type) {
+      store.isLoading = false;
+      axios.get(store.apiUrl + type).then((results) => {
+        if (type == "posts") {
+          store[type] = results.data;
+        } else if (type == "profiles") {
+          store[type] = results.data;
+        }
+        store.isLoading = true;
+      });
+    },
+  },
+  mounted() {
+    this.getApi("posts");
+    this.getApi("profiles");
   },
 };
 </script>
