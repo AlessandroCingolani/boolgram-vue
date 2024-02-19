9b5a8296a3fe624fdb48002767aa8059c192e803
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-19 17:03:25 +0100

    [change]:naming apiUrl

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  apiUrli : 'https://flynn.boolean.careers/exercises/api/boolgram/',
+  apiUrl : 'https://flynn.boolean.careers/exercises/api/boolgram/',
   posts:[],
   profiles:[],
   isLoading: false,
