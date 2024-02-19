548c05ff4a4499d4efe441813e1ae3b54b38c84f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-19 18:37:10 +0100

    [add]:store and cicle

--- src/components/BlockPosts.vue
+++ src/components/BlockPosts.vue
@@ -1,16 +1,23 @@
 <script>
 import CardPost from "./partials/CardPost.vue";
+import { store } from "../data/store";
 export default {
   name: "BlockPosts",
   components: {
     CardPost,
+  },
+
+  data() {
+    return {
+      store,
+    };
   },
 };
 </script>
 
 <template>
   <div class="posts-container">
-    <CardPost />
+    <CardPost v-for="post in store.posts" :key="post.id" :post="post" />
   </div>
 </template>
 
