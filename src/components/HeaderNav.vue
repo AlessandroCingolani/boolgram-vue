fda9ade30c5c9e0cccce19ad37cbeab17e9a373e
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-20 11:54:31 +0100

    [add]:minor graphic changes

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -12,11 +12,16 @@ export default {
           <img src="/img/logo.png" alt="logo" />
         </div>
         <div class="search">
-          <input class="form-control" type="text" placeholder="Search" />
+          <input
+            class="form-control"
+            style="text-align: center"
+            type="text"
+            placeholder="Search"
+          />
         </div>
         <div class="nav-profile d-flex align-items-center">
           <i class="fa-regular fa-heart"></i>
-          <i class="fa-solid fa-house"></i>
+          <i class="fa-solid fa-house mx-3"></i>
           <div class="small-profile">
             <img src="/img/profile.jpg" alt="profile" />
           </div>
@@ -41,6 +46,16 @@ export default {
   overflow: hidden;
   img {
     width: 100%;
+  }
+}
+
+.nav-profile {
+  i {
+    font-size: 1.2rem;
+    &:hover {
+      cursor: pointer;
+      scale: 1.1;
+    }
   }
 }
 </style>
